58ec2103b6a9b92a557a332b87c8b71ba90848f1
yomajo <ed.vinas@yahoo.com>
2020-06-12 11:16:23 +0300

    cleanup, simplified dates format to report

--- Helper Files/orders_report.py
+++ Helper Files/orders_report.py
@@ -1,7 +1,8 @@
-from amzn_parser_utils import dkey_to_float ,is_windows_machine
+from amzn_parser_utils import dkey_to_float, is_windows_machine, simplify_date
 from amzn_parser_constants import TEMPLATE_SHEET_MAPPING
 import logging
 import openpyxl
+import os
 # Delete later
 import json
 import subprocess
@@ -17,7 +18,7 @@ class OrdersReport():
     Error handling is present outside of this class.
     
     Main method: export() - creates individual sheets, pushes selected data from corresponding orders;
-    creates summary sheet, calculates regional / currency'''
+    creates summary sheet, calculates regional / currency based totals'''
     
     def __init__(self, export_obj : dict):
         self.export_obj = export_obj
@@ -26,48 +27,8 @@ class OrdersReport():
         '''generator yielding self.export_obj dict KEY1 (region) and nested KEY2 (currency) at a time'''
         for region in self.export_obj:
             for currency in self.export_obj[region]:
-                # print(f'Region {region} contains currency: {currency}')
-                # print(f'Inside this contains object of type: {type(self.export_obj[region][currency])}, which has {len(self.export_obj[region][currency])} members')
                 yield region, currency
-
-    def export_wb(self, wb_name : str):
-        '''creates a workbook from thin air'''
-        try:
-            self.wb = openpyxl.Workbook()
-            ws = self.wb.active
-            ws.title = 'Summary'
-            # Unpacking data to sheets:
-            for region, currency in self.unpack_export_obj():
-                working_orders = self.export_obj[region][currency]
-                sheet_ready_data = self.apply_orders_to_sheet_format(working_orders)
-                self._data_to_sheet(f'{region} {currency}', sheet_ready_data)
-
-        except Exception as e:
-            print(f'Errors while creating excel file. Err: {e}')
-        finally:
-            self.wb.save(wb_name)
-            self.wb.close()
     
-
-    def _data_to_sheet(self, ws_name : str, orders_data: list):
-        '''creates new ws_name sheet and fills it with orders_data argument'''
-        self._create_sheet(ws_name)
-        active_ws = self.wb[ws_name]
-        active_ws.freeze_panes = active_ws['A2']
-        self._fill_sheet(active_ws, orders_data)
-
-
-    def _create_sheet(self, ws_name : str):
-        '''creates new sheet obj with name ws_name'''
-        self.wb.create_sheet(title=ws_name)
-
-    def _fill_sheet(self, active_ws, orders_data:list):
-        '''writes headers, and corresponding orders data to active_ws'''
-        self._write_headers(active_ws, SHEET_HEADERS)
-        self._write_sheet_orders(active_ws, SHEET_HEADERS, orders_data)
-
-# --------------------------------------------------------------------------------
-
     def apply_orders_to_sheet_format(self, raw_orders_data : list):
         '''reduces input data to that needed in output data sheet'''
         ready_orders_data = []
@@ -82,12 +43,29 @@ class OrdersReport():
             if d_key in ['item-price', 'item-tax', 'shipping-price', 'shipping-tax']:
                 d_value_as_float = dkey_to_float(order_dict, d_key)
                 d_with_output_keys[header] = d_value_as_float
+            elif d_key in ['purchase-date', 'payments-date']:
+                simpler_date = simplify_date(order_dict[d_key])
+                d_with_output_keys[header] = simpler_date
             else:
                 d_with_output_keys[header] = order_dict[TEMPLATE_SHEET_MAPPING[header]]
         return d_with_output_keys
-    
 
-    #  ---------------------------------------------------------------------------
+    def _data_to_sheet(self, ws_name : str, orders_data: list):
+        '''creates new ws_name sheet and fills it with orders_data argument'''
+        self._create_sheet(ws_name)
+        active_ws = self.wb[ws_name]
+        active_ws.freeze_panes = active_ws['A2']
+        self._fill_sheet(active_ws, orders_data)
+        # self._adjust_col_widths(active_ws)
+
+    def _create_sheet(self, ws_name : str):
+        '''creates new sheet obj with name ws_name'''
+        self.wb.create_sheet(title=ws_name)
+
+    def _fill_sheet(self, active_ws, orders_data:list):
+        '''writes headers, and corresponding orders data to active_ws'''
+        self._write_headers(active_ws, SHEET_HEADERS)
+        self._write_sheet_orders(active_ws, SHEET_HEADERS, orders_data)
 
     @staticmethod
     def _write_headers(worksheet, headers):
@@ -103,46 +81,43 @@ class OrdersReport():
     def _write_sheet_orders(self, worksheet, headers, orders_data):
         for row, col in self.range_generator(orders_data, headers):
             working_dict = orders_data[row]
-            # Line saving to functions:
-            # fake_pointer = TEMPLATE_SHEET_MAPPING[headers[col]]
-            # print(f'I would get value with this key in orders_dict: {fake_pointer}')
-
             key_pointer = headers[col]
             # offsets due to excel vs python numbering  + headers in row 1
             worksheet.cell(row + 2, col + 1).value = working_dict[key_pointer]
 
-
-    def export(self, report_path : str):
-        # Silencing error ------------
-        self.etonas_orders = []
-        # Silencing error ------------
-        reheaded_etonas_orders = self.apply_orders_to_sheet_format(self.etonas_orders)
-        try:
-            wb = openpyxl.Workbook()
-            ws = wb.active
-            self._write_headers(ws, SHEET_HEADERS)
-            self._write_sheet_orders(ws, SHEET_HEADERS, reheaded_etonas_orders)
-            # self.adjust_col_widths(ws)
-        except Exception as e:
-            print(f'Error occured while creating report. Err: {e}. Saving, exiting')
-        finally:
-            wb.save(report_path)
-
-    # def adjust_col_widths(self, worksheet):
-    #     for col in worksheet.columns:
-    #         max_length = 0
-    #         col_letter = col[0].column_letter
-    #         for cell in col:
-    #             try:
-    #                 if len(str(cell.value)) > max_length:
-    #                     max_length = len(cell.value)
-    #             except:
-    #                 pass
-    #         adjusted_width = (max_length + 2) * 1.1
-    #         worksheet.column_dimensions[col_letter].width = adjusted_width
+    def _adjust_col_widths(self, worksheet):
+        for col in worksheet.columns:
+            max_length = 0
+            col_letter = col[0].column_letter
+            for cell in col:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2) * 1.1
+            worksheet.column_dimensions[col_letter].width = adjusted_width
 
     def fill_summary(self):
         pass
+
+    def export(self, wb_name : str):
+        '''unpacks data object and pushes orders data to appropriate sheets, forms a summary sheet, saves new workbook'''
+        try:
+            self.wb = openpyxl.Workbook()
+            ws = self.wb.active
+            ws.title = 'Summary'
+            # Unpacking data to sheets:
+            for region, currency in self.unpack_export_obj():
+                working_orders = self.export_obj[region][currency]
+                sheet_ready_data = self.apply_orders_to_sheet_format(working_orders)
+                self._data_to_sheet(f'{region} {currency}', sheet_ready_data)
+        except Exception as e:
+            print(f'Unknown error while creating excel report {os.path.basename(wb_name)}. Err: {e}')
+        finally:
+            self.wb.save(wb_name)
+            self.wb.close()
+
 
 def read_json(fname:str):
     with open(fname, 'r', encoding='utf-8') as f:
@@ -156,11 +131,9 @@ def run():
     excel_file = 'EXCEL REPORT.xlsx'
     data_obj = read_json('export_data.json')
     report = OrdersReport(data_obj)
-    report.export_wb(excel_file)
-    print('File saved. Or not. Check.')
-
+    report.export(excel_file)
+    print('File saved. Check.')
     open_file(excel_file)
-    # report.export('EXCEL REPORT.xlsx')
 
 
 if __name__ == "__main__":
