b8391b633601098d4966cbb2059147acb198089d
yomajo <ed.vinas@yahoo.com>
2020-06-12 11:20:47 +0300

    reordered columns in data sheets

--- Helper Files/amzn_parser_constants.py
+++ Helper Files/amzn_parser_constants.py
@@ -1,13 +1,13 @@
 TEMPLATE_SHEET_MAPPING= {
         'Order ID' : 'order-id',
         'Order Item ID' : 'order-item-id',
-        'Purchase Date' : 'purchase-date',
-        'Payment Date': 'payments-date',
         'Repicient' : 'recipient-name',
         'Currency' : 'currency',
         'Quantity' : 'quantity-purchased',
         'Item Price' : 'item-price',
         'Item Tax' : 'item-tax',
         'Shipping Price' : 'shipping-price',
-        'Shipping Tax' : 'shipping-tax'
+        'Shipping Tax' : 'shipping-tax',
+        'Purchase Date' : 'purchase-date',
+        'Payment Date': 'payments-date'
 }
