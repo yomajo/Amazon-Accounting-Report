64e68794ae9c0bea24b094a0494bdf3ba67503cf
yomajo <ed.vinas@yahoo.com>
2020-06-12 12:44:58 +0300

    new func returning worksheet col letter from idx

--- Helper Files/amzn_parser_utils.py
+++ Helper Files/amzn_parser_utils.py
@@ -1,3 +1,4 @@
+from openpyxl.utils import get_column_letter
 from datetime import datetime
 import platform
 import logging
@@ -102,6 +103,13 @@ def simplify_date(date_str : str) -> str:
     except ValueError:
         logging.warning(f'Unable to return simplified version of date: {date_str}. Returning raw format instead')
         return date_str
+
+def col_to_letter(col : int, zero_indexed=True) -> str:
+    '''returns column letter from worksheet column index'''
+    if zero_indexed:
+        col += 1
+    return get_column_letter(col)
+
 
 if __name__ == "__main__":
     pass
