5836ae3064f9647491d481645755029cea6e12fd
yomajo <ed.vinas@yahoo.com>
2020-06-15 17:32:12 +0300

    -add daily breakdown table -summary sheet styling -integrating with rest of program - deleted unnecc. imports - new styling global variables - unified naming convention worksheet as ws -  summary sheet adjusting col widhts - variable factor for widths - coloring of headers in summary sheet - deleted error handling (outsourced to parse_orders.py) - deleted helper funcs parsing json, opening report

--- Helper Files/orders_report.py
+++ Helper Files/orders_report.py
@@ -1,16 +1,17 @@
 from amzn_parser_utils import is_windows_machine, simplify_date, col_to_letter
 from amzn_parser_constants import TEMPLATE_SHEET_MAPPING
 from collections import defaultdict
-import logging
+import itertools
 import openpyxl
 import os
-# Delete later
-import json
-import subprocess
 
 # GLOBAL VARIABLES
 VBA_ERROR_ALERT = 'ERROR_CALL_DADDY'
 SUMMARY_SHEET_NAME = 'Summary'
+D_BREAKDOWN_HEADERS = ['Segment', 'Date', 'Total', 'Orders', 'Average']
+BOLD_STYLE = openpyxl.styles.Font(bold=True, name='Calibri')
+BACKGROUND_COLOR_STYLE = openpyxl.styles.PatternFill(fgColor='D6DEFF',fill_type='solid')
+THIN_BORDER = openpyxl.styles.Side(border_style='thin')
 SHEET_HEADERS = list(TEMPLATE_SHEET_MAPPING.keys())
 
 
@@ -130,10 +131,10 @@ class OrdersReport():
         self._write_headers(active_ws)
         self._write_sheet_orders(active_ws, orders_data)
 
-    def _write_headers(self, worksheet):
+    def _write_headers(self, ws):
         for col, header in enumerate(SHEET_HEADERS):
             self._update_col_widths(col, header)
-            worksheet.cell(1, col + 1).value = header
+            ws.cell(1, col + 1).value = header
 
     @staticmethod
     def range_generator(orders_data, headers):
@@ -141,17 +142,17 @@ class OrdersReport():
             for col, _ in enumerate(headers):
                 yield row, col
     
-    def _write_sheet_orders(self, worksheet, orders_data : list):
+    def _write_sheet_orders(self, ws, orders_data : list):
         for row, col in self.range_generator(orders_data, SHEET_HEADERS):
             working_dict = orders_data[row]
             key_pointer = TEMPLATE_SHEET_MAPPING[SHEET_HEADERS[col]]
             self._update_col_widths(col, str(working_dict[key_pointer]))
             # offsets due to excel vs python numbering  + headers in row 1
-            worksheet.cell(row + 2, col + 1).value = working_dict[key_pointer]
+            ws.cell(row + 2, col + 1).value = working_dict[key_pointer]
 
-    def _update_col_widths(self, col : int, cell_value : str):
+    def _update_col_widths(self, col : int, cell_value : str, zero_indexed=True):
         '''runs on each cell. Forms a dictionary {'A':30, 'B':15...} for max column widths in worksheet (width as length of max cell)'''
-        col_letter = col_to_letter(col)
+        col_letter = col_to_letter(col, zero_indexed=zero_indexed)
         if col_letter in self.col_widths:
             # check for length, update if current cell length exceeds current entry for column
             if len(cell_value) > self.col_widths[col_letter]:
@@ -159,72 +160,120 @@ class OrdersReport():
         else:
             self.col_widths[col_letter] = len(cell_value)
 
-    def _adjust_col_widths(self, worksheet, col_widths : dict):
-        '''iterates over {'A':30, 'B':40, 'C':35...} dict to resize worksheets' column widths'''
+    def _adjust_col_widths(self, ws, col_widths : dict, summary=False):
+        '''iterates over {'A':30, 'B':40, 'C':35...} dict to resize worksheets' column widths. Summary ws wider columns with summary=True'''
+        factor = 1.3 if summary else 1.05
         for col_letter in col_widths:
-            adjusted_width = ((col_widths[col_letter]) + 2 * 1.05 )
-            worksheet.column_dimensions[col_letter].width = adjusted_width
+            adjusted_width = ((col_widths[col_letter] + 2) * factor)
+            ws.column_dimensions[col_letter].width = adjusted_width
     
     def fill_summary(self):
         '''Forms a summary statistics sheet / report'''
         active_ws = self.wb[SUMMARY_SHEET_NAME]
+        self.col_widths = {}
         self.write_totals_table(active_ws)
-        self.write_daily_breakdown_table(active_ws)        
+        self.write_daily_breakdown_table(active_ws)
+        self._adjust_col_widths(active_ws, self.col_widths, summary=True)
+        self.color_table_headers(active_ws)
         
     def write_totals_table(self, ws):
         '''fills out first table in main sheet of report.
         Table height is fixed, but width is flexible depending on data segments'''
+        ws.cell(1, 3).value = 'Totals'
+        ws.cell(1, 3).font = BOLD_STYLE
         # Write vertical headers (Total, Orders, Average in A3:A5)
         any_values_dict = list(self.totals.values())[0]
         for row, values_header in enumerate(any_values_dict.keys()):
+            self._update_col_widths(1, values_header)
             ws.cell(row + 3, 1).value = values_header
         # Fill segment name and values (range B2:[n]5):
-        for col, (segment, values) in enumerate(self.totals.items()):
+        for col, (segment, stats) in enumerate(self.totals.items()):
+            self._update_col_widths(col + 2, segment, zero_indexed=False)
             ws.cell(2, col + 2).value = segment
-            ws.cell(3, col + 2).value = values['Total']
-            ws.cell(4, col + 2).value = values['Orders']
-            ws.cell(5, col + 2).value = values['Average']
-    
+            ws.cell(2, col + 2).font = BOLD_STYLE
+            # Skipping update col_widths. Numbers unlikely to break out beyond header titles
+            ws.cell(3, col + 2).value = stats['Total']
+            ws.cell(3, col + 2).number_format = '#,##0.00'
+            ws.cell(4, col + 2).value = stats['Orders']
+            ws.cell(5, col + 2).value = stats['Average']
+            ws.cell(5, col + 2).number_format = '#,##0.00'
+
+
     def write_daily_breakdown_table(self, ws):
         '''fills out second table, which breaks down daily statistics for each segment'''
-        ws.cell(10, 3).value = 'Somewhere here will come the more powerful table.'
-        # pass
+        self.row_cursor = 8
+        ws.cell(self.row_cursor - 1, 3).value = 'Daily Breakdown'
+        ws.cell(self.row_cursor - 1, 3).font = BOLD_STYLE
+        self._write_d_breakdown_headers(ws)
+        self._write_d_breakdown_data(ws)
+
+    def _write_d_breakdown_headers(self, ws):
+        '''writes fixed headers for second table in summary sheet'''
+        for col, d_breakdown_header in enumerate(D_BREAKDOWN_HEADERS):
+            self._update_col_widths(col + 1, d_breakdown_header)
+            ws.cell(self.row_cursor, col + 1).value = d_breakdown_header
+            ws.cell(self.row_cursor, col + 1).font = BOLD_STYLE
+        self.row_cursor += 1
+
+    def _write_d_breakdown_data(self, ws):
+        '''iterates self.daily_breakdown_obj and fills second table in summary sheet with data'''
+        for region, daily_breakdown in self.daily_breakdown_obj.items():
+            # Entering and writing segment
+            self._update_col_widths(1, region, zero_indexed=False)
+            ws.cell(self.row_cursor, 1).value = region
+            self.__apply_horizontal_line(ws, self.row_cursor)            
+            for date, stats in daily_breakdown.items():
+                # Entering and writing daily breakdown stats
+                self._update_col_widths(2, date, zero_indexed=False)
+                ws.cell(self.row_cursor, 2).value = date
+                self.__write_date_stats(ws, stats, self.row_cursor)
+                self.row_cursor += 1
+    
+    @staticmethod
+    def __write_date_stats(ws, stats : dict, row : int):
+        '''writes Total, Orders and Average fields in Summary Daily Breakdown table. Takes args: ws (worksheet obj);
+        stats in format: {'Total':X, 'Orders':Y, 'Average':Z}''' 
+        ws.cell(row, 3).value = stats['Total']
+        ws.cell(row, 3).number_format = '#,##0.00'
+        ws.cell(row, 4).value = stats['Orders']
+        ws.cell(row, 5).value = stats['Average']
+        ws.cell(row, 5).number_format = '#,##0.00'
+
+    def __apply_horizontal_line(self, ws, row):
+        '''adds horizonal line through A:E at given row top'''
+        for col in range(1, 6):
+            ws.cell(row, col).border = openpyxl.styles.Border(top=THIN_BORDER)
+    
+    def color_table_headers(self, ws):
+        '''Colors fixed ranges defined in generator {}'''
+        for row, col in itertools.chain(self.color_headers_table1(), self.color_headers_table2()):
+            ws.cell(row, col).fill = BACKGROUND_COLOR_STYLE
+    
+    def color_headers_table1(self):
+        '''generator for Totals headers coloring (adjustable width dependent on segments count)'''
+        color_cols =  len(list(self.totals.keys())) + 2
+        for row in [1, 2]:
+            for col in range(1, color_cols):
+                yield row, col
+    
+    def color_headers_table2(self):
+        '''generator for daily breakdown headers coloring'''
+        for row in [7, 8]:
+            for col in range(1, 6):
+                yield row, col
 
     def export(self, wb_name : str):
         '''Creates workbook, and exports class objects: segments_orders_obj, daily_breakdown_obj, totals to
         segment worksheets and creates report summary sheet, saves new workbook'''
-        try:
-            self.wb = openpyxl.Workbook()
-            ws = self.wb.active
-            ws.title = SUMMARY_SHEET_NAME
-            for segment, segment_orders in self.segments_orders_obj.items():
-                self._data_to_sheet(segment, segment_orders)
-            self.fill_summary()
-        except Exception as e:
-            print(f'Unknown error while creating excel report {os.path.basename(wb_name)}. Err: {e}')
-        finally:
-            self.wb.save(wb_name)
-            self.wb.close()
-
-
-def read_json(fname:str):
-    with open(fname, 'r', encoding='utf-8') as f:
-        contents = json.load(f)
-    return contents
-
-def open_file(fname:str):
-    subprocess.call(['xdg-open',fname])
-
-def run():
-    excel_file = 'EXCEL REPORT.xlsx'
-    data_obj = read_json('export_data.json')
-    report = OrdersReport(data_obj)
-    report.export(excel_file)
-    print('File saved. Check.')
-    open_file(excel_file)
+        self.wb = openpyxl.Workbook()
+        ws = self.wb.active
+        ws.title = SUMMARY_SHEET_NAME
+        for segment, segment_orders in self.segments_orders_obj.items():
+            self._data_to_sheet(segment, segment_orders)
+        self.fill_summary()
+        self.wb.save(wb_name)
+        self.wb.close()
 
 
 if __name__ == "__main__":
-    subprocess.call(['rm', 'EXCEL REPORT.xlsx'])
-    run()
-    # pass
+    pass
